b9964ed92227df70fc7fa094198960050b7525fa
SPARTACUS5329 <adityaspartacus5329@gmail.com>
2024-11-29 19:47:36 +0530

    lexer working for input grammar

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,9 +1,27 @@
-use crate::utils;
+use crate::utils::{self, NiceError};
 use regex::Regex;
 use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
+#[derive(PartialEq, Debug)]
+pub enum LexerNodeE {
+    LexerNodeRoot,
+    LexerNodeTerminal,
+    LexerNodeRegular,
+}
+
+impl Clone for LexerNodeE {
+    fn clone(&self) -> Self {
+        match self {
+            LexerNodeE::LexerNodeRoot => LexerNodeE::LexerNodeRoot,
+            LexerNodeE::LexerNodeTerminal => LexerNodeE::LexerNodeTerminal,
+            LexerNodeE::LexerNodeRegular => LexerNodeE::LexerNodeRegular,
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct LexerNode {
+    pub kind: LexerNodeE,
     pub id: i32,
     pub next: HashMap<String, Rc<RefCell<LexerNode>>>,
 }
@@ -11,18 +29,19 @@ pub struct LexerNode {
 impl Clone for LexerNode {
     fn clone(&self) -> Self {
         LexerNode {
+            kind: self.kind.clone(),
             id: self.id,
             next: self.next.clone(),
         }
     }
 }
 
-pub fn construct_lexer_trie() -> Result<Rc<RefCell<LexerNode>>, String> {
+pub fn construct_lexer_trie() -> Result<Rc<RefCell<LexerNode>>, NiceError> {
     let lexer_grammar_filename = String::from("./assets/lexer_grammar.txt");
     let mut node_map = HashMap::<i32, Rc<RefCell<LexerNode>>>::new();
 
     let Ok(lines) = utils::read_lines(&lexer_grammar_filename) else {
-        return Err("Error in readling lexer grammar file".to_string());
+        return Err(NiceError::new("Error in readling lexer grammar file".to_string()));
     };
 
     let mut node_count = 0i32;
@@ -30,6 +49,11 @@ pub fn construct_lexer_trie() -> Result<Rc<RefCell<LexerNode>>, String> {
         node_count += 1;
         let index = i as i32;
         let node = Rc::new(RefCell::new(LexerNode {
+            kind: if index == 0 {
+                LexerNodeE::LexerNodeRoot
+            } else {
+                LexerNodeE::LexerNodeRegular
+            },
             id: index,
             next: HashMap::<String, Rc<RefCell<LexerNode>>>::new(),
         }));
@@ -38,23 +62,22 @@ pub fn construct_lexer_trie() -> Result<Rc<RefCell<LexerNode>>, String> {
     }
 
     let terminal_node = Rc::new(RefCell::new(LexerNode {
+        kind: LexerNodeE::LexerNodeTerminal,
         id: node_count,
         next: HashMap::<String, Rc<RefCell<LexerNode>>>::new(),
     }));
 
     for line in lines {
         let Some((node_number, edges)) = line.split_once(":") else {
-            eprint!("Invalid lexer grammar: Cannot read node id");
-            return Err("Invalid lexer grammar".to_string());
+            return Err(NiceError::new("Invalid lexer grammar: Cannot read node id".to_string()));
         };
         let node_id = match node_number.parse::<i32>() {
             Ok(num) => num,
-            Err(_) => return Err("Error in parsing node_id".to_string()),
+            Err(_) => return Err(NiceError::new("Error in parsing node_id".to_string())),
         };
 
         let Some(node) = node_map.get(&node_id) else {
-            eprintln!("Invalid node_id");
-            return Err("Invalid node_id".to_string());
+            return Err(NiceError::new("Invalid node_id".to_string()));
         };
 
         let edge_pairs: Vec<String> = edges.split(";;").map(String::from).collect();
@@ -63,8 +86,7 @@ pub fn construct_lexer_trie() -> Result<Rc<RefCell<LexerNode>>, String> {
             let trimmed_pair = pair.trim();
             let re = Regex::new(r"(?x) \(\s* (.+) \s*,\s* (.+) \s*\) ").unwrap();
             let Some(captures) = re.captures(trimmed_pair) else {
-                eprintln!("Invalid lexer grammar: Cannot read graph pairs: {}, {}", node.borrow().id, pair);
-                return Err("Invalid lexer grammar".to_string());
+                return Err(NiceError::new(format!("Invalid lexer grammar: Cannot read graph pairs: {}, {}", node.borrow().id, pair)));
             };
 
             let char = captures.get(1).map_or("", |m| m.as_str());
@@ -74,13 +96,11 @@ pub fn construct_lexer_trie() -> Result<Rc<RefCell<LexerNode>>, String> {
                 "#" => Rc::clone(&terminal_node),
                 _ => {
                     let Ok(neighbour_id) = neighbour_str.parse::<i32>() else {
-                        eprintln!("Error parsing neighbour_id");
-                        return Err("Error parsing neighbour_id".to_string());
+                        return Err(NiceError::new("Error parsing neighbour_id".to_string()));
                     };
 
                     let Some(neighbour) = node_map.get(&neighbour_id) else {
-                        eprintln!("Invalid neighbour_id");
-                        return Err("Invalid neighbour_id".to_string());
+                        return Err(NiceError::new("Invalid neighbour_id".to_string()));
                     };
 
                     Rc::clone(&neighbour)
@@ -95,7 +115,7 @@ pub fn construct_lexer_trie() -> Result<Rc<RefCell<LexerNode>>, String> {
 
     let root = match node_map.get(&0) {
         Some(root) => Rc::clone(&root),
-        _ => return Err("Root node not found".to_string()),
+        _ => return Err(NiceError::new("Root node not found".to_string())),
     };
 
     Ok(root)
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,11 +1,23 @@
 use std::fs::OpenOptions;
 use std::io::{self, Read};
 
+pub struct NiceError {
+    message: String,
+}
+
+impl NiceError {
+    pub fn new(message: String) -> NiceError {
+        let error = NiceError { message };
+        eprintln!("{}", error.message);
+        error
+    }
+}
+
 pub fn read_file(filename: &String) -> io::Result<String> {
     let mut file = match OpenOptions::new().read(true).open(filename) {
         Ok(file) => file,
         Err(error) => {
-            eprintln!("Error opening file: {:?}", filename);
+            eprintln!("Error opening file: {:?}", error);
             return Err(error);
         }
     };
